e66bbd96bf2a6b727f60ff669e6409349fe49e26
dtereshchenko <di.tereshenko@gmail.com>
2016-01-07 22:50:01 +0200

    Initial commit

---
+++ app/pages/item-details/item-details.js
@@ -0,0 +1,21 @@
+import {IonicApp, Page, Alert, NavController, NavParams} from 'ionic/ionic';
+
+@Page({
+    templateUrl: 'build/pages/item-details/item-details.html'
+})
+export class ItemDetailsPage {
+    constructor(app:IonicApp, nav:NavController, navParams:NavParams) {
+        this.nav = nav;
+        // If we navigated to this page, we will have an item available as a nav param
+        this.selectedItem = navParams.get('item');
+        console.log('details');
+        console.dir(navParams);
+    }
+
+    subscribe () {
+        Alert.create({
+            title: 'Thanks!',
+            subTitle: 'You have succesfully subscribed to "Walkind dead" TV series.'
+        });
+    }
+}
