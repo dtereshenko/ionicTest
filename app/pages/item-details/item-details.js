5aab01f0945dd3fc81f20a7553c6d9c55506bca4
dtereshchenko <di.tereshenko@gmail.com>
2016-01-24 18:48:00 +0200

    Add showMore, showLess actions for description. Fix episodes watched toggle action.

--- app/pages/item-details/item-details.js
+++ app/pages/item-details/item-details.js
@@ -7,19 +7,41 @@ import {NgClass} from 'angular2/common';
     directives: [NgClass]
 })
 export class ItemDetailsPage {
+    descriptionShortLength = 250;
+    descriptionLongLength = 1000;
+
+
     constructor(nav:NavController, navParams:NavParams, seriesDetailService: SeriesDetailsService) {
         this.nav = nav;
+
 
         // If we navigated to this page, we will have an item available as a nav param
         this.selectedItemId = navParams.get('id');
         this.seriesDetailService = seriesDetailService;
         this.seriesDetailService.getDetail(this.selectedItemId).then((item) => {
             this.item = item;
-            console.log('Detailed data received.');
+            this.descriptionText = this.getDescription(this.descriptionShortLength);
         });
     }
 
-    ToggleSubscribe (item) {
+    showMore () {
+        this.descriptionText = this.getDescription(this.descriptionLongLength);
+    }
+
+    showLess () {
+        this.descriptionText = this.getDescription(this.descriptionShortLength);
+    }
+
+    getDescription (descriptionLength) {
+        let description = this.item.description;
+
+        if (description.length > descriptionLength) {
+            return description.substr(0, descriptionLength - 3) + '...';
+        }
+        return description;
+    }
+
+    toggleSubscribe (item) {
         item.subscribed = !item.subscribed;
         let subscribeAlert = Alert.create({
             title: item.subscribed ? 'Thanks!' : 'Attention!',
@@ -30,7 +52,7 @@ export class ItemDetailsPage {
         this.nav.present(subscribeAlert)
     }
 
-    ToggleEpisode (season, episode) {
-        this.item.watched[season][episode] = !this.item.watched[season][episode];
+    toggleEpisode (season, episode) {
+        this.item.watched[season][episode].w = !this.item.watched[season][episode].w;
     }
 }
--- app/pages/item-details/series.detail.mock.js
+++ app/pages/item-details/series.detail.mock.js
@@ -4,23 +4,19 @@
 export var DETAILS = {
     'Walking Dead': {
         title: 'Walking Dead',
-        description: `Based on the comic book series written by Robert Kirkman, this gritty drama portrays life in the weeks
-            and months
-            following a zombie apocalypse. Led by police officer Rick Grimes, his family and a group of other
-            survivors find
-            themselves constantly on the move in search of a safe and secure home. Bu…`,
+        description: `Based on the comic book series written by Robert Kirkman, this gritty drama portrays life in the weeks and months following a zombie apocalypse. Led by police officer Rick Grimes, his family and a group of other survivors find themselves constantly on the move in search of a safe and secure home. But the pressure each day to stay alive sends many in the group to the deepest depths of human cruelty, and Rick discovers that the overwhelming fear of the survivors can be more deadly than the zombies walking among them.`,
         author: 'Sam Pin',
         release: 2009, subscribed: true,
         thumbnail: 'http://orig07.deviantart.net/84fa/f/2013/346/8/5/the_walking_dead__season_two___icon_by_blagoicons-d6xp5km.png',
-        watched: [[false, false, false, false, false, false, false, false, false, false, false, false], [false, false, false, false, false, false]]
+        watched: [[{w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false},  {w: false}], [{w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}]]
     },
     'Game of Thrones': {
         title: 'Game of Thrones',
-        description: `George R.R. Martin's best-selling book series "A Song of Ice and Fire" is brought to the screen as HBO sinks its considerable storytelling teeth into the medieval fantasy epic. It's the depiction of two powerful families -- kings and queens, knights and renegades, liars and honest men -- playing a d…`,
+        description: `George R.R. Martin's best-selling book series "A Song of Ice and Fire" is brought to the screen as HBO sinks its considerable storytelling teeth into the medieval fantasy epic. It's the depiction of two powerful families -- kings and queens, knights and renegades, liars and honest men -- playing a deadly game for control of the Seven Kingdoms of Westeros, and to sit atop the Iron Throne. Martin is credited as a co-executive producer and one of the writers for the series, which was filmed in Northern Ireland and Malta.`,
         author: 'George R. R. Martin',
         release: 2011, subscribed: false,
         thumbnail: 'http://cdn.warer.com/media/Game-of-Thrones-seven-kingdoms.png',
-        watched: [[false, false, false, false, false, false, false, false, false, false, false], [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false], [false, false, false, false, false, false], [false, false, false, false, false, false, false, false, false, false]]
+        watched: [[{w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false},  {w: false}], [{w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false},  {w: false}], [{w: false}, {w: false}, {w: false}, {w: false}, {w: false},  {w: false}], [{w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}]]
     },
     'American Horror Story': {
         title: 'American Horror Story',
@@ -28,7 +24,7 @@ export var DETAILS = {
         author: 'George R. R. Martin',
         release: 2011, subscribed: false,
         thumbnail: 'http://www.kerryherta.com/_Media/ahsh_poster_mattresskeyface_hr.jpeg',
-        watched: [[false, false, false, false, false, false, false, false, false, false, false], [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false], [false, false, false, false, false, false], [false, false, false, false, false, false, false, false, false, false]]
+        watched: [[{w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false},  {w: false}], [{w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false},  {w: false}], [{w: false}, {w: false}, {w: false}, {w: false}, {w: false},  {w: false}], [{w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}]]
     },
     'Sherlock': {
         title: 'Sherlock',
@@ -36,7 +32,7 @@ export var DETAILS = {
         author: ' Mark Gatiss, Steven Moffat',
         release: 2011, subscribed: false,
         thumbnail: 'https://www.hit2k.com/wp-content/uploads/2015/07/the_testament_of_sherlock_holmes_Hit2k.png',
-        watched: [[false, false, false, false, false, false, false, false, false, false, false], [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false], [false, false, false, false, false, false], [false, false, false, false, false, false, false, false, false, false]]
+        watched: [[{w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false},  {w: false}], [{w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false},  {w: false}], [{w: false}, {w: false}, {w: false}, {w: false}, {w: false},  {w: false}], [{w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}]]
     },
     'The Big Bang Theory': {
         title: 'The Big Bang Theory',
@@ -44,7 +40,7 @@ export var DETAILS = {
         author: ' Chuck Lorre, Bill Prady',
         release: 2007, subscribed: false,
         thumbnail: 'http://orig01.deviantart.net/0eeb/f/2014/095/d/7/the_big_bang_theory_folder_icon_by_andreas86-d6pff2g.png',
-        watched: [[false, false, false, false, false, false, false, false, false, false, false], [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false], [false, false, false, false, false, false], [false, false, false, false, false, false, false, false, false, false]]
+        watched: [[{w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false},  {w: false}], [{w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false},  {w: false}], [{w: false}, {w: false}, {w: false}, {w: false}, {w: false},  {w: false}], [{w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}]]
     },
     'Fargo': {
         title: 'Fargo',
@@ -52,7 +48,7 @@ export var DETAILS = {
         author: 'George R. R. Martin',
         release: 2014, subscribed: false,
         thumbnail: 'http://www.weeatfilms.com/wp-content/uploads/2016/01/fargo-season-2-390x280.jpg',
-        watched: [[false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false], [false, false, false, false, false, false, false, false, false, false]]
+        watched: [[{w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false},  {w: false}], [{w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}]]
     },
     'Supernatural': {
         title: 'Supernatural',
@@ -60,7 +56,7 @@ export var DETAILS = {
         author: 'Eric Kripke',
         release: 2005, subscribed: false,
         thumbnail: 'http://orig12.deviantart.net/546f/f/2014/343/0/b/supernatural___v8_by_rest_in_torment-d89a0vr.png',
-        watched: [[false, false, false, false, false, false, false, false, false, false, false], [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false], [false, false, false, false, false, false], [false, false, false, false, false, false, false, false, false, false]]
+        watched: [[{w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false},  {w: false}], [{w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false},  {w: false}], [{w: false}, {w: false}, {w: false}, {w: false}, {w: false},  {w: false}], [{w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}]]
     },
     'Gray\'s Anatomy': {
         title: 'Gray\'s Anatomy',
@@ -68,6 +64,6 @@ export var DETAILS = {
         author: 'Shonda Rhimes',
         release: 2005, subscribed: false,
         thumbnail: 'http://www.veryicon.com/icon/png/Movie%20%26%20TV/TV%20Shows/Greys%20Anatomy.png',
-        watched: [[false, false, false, false, false, false, false, false, false, false, false], [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false], [false, false, false, false, false, false], [false, false, false, false, false, false, false, false, false, false]]
+        watched: [[{w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false},  {w: false}], [{w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false},  {w: false}], [{w: false}, {w: false}, {w: false}, {w: false}, {w: false},  {w: false}], [{w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}, {w: false}]]
     }
 };
